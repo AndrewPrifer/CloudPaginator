1e857d8b515847ba59b7104fcc0c0a447a0255c2
Andrew Peter Prifer <prifer.andras@gmail.com>
2016-12-12 19:37:38 +0100

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var ejs = require('ejs');
+
+var cheerio = require('cheerio');
+
+router.get('/', function(req, res) {
+  res.redirect('/paginate-html');
+});
+
+router.post('/paginate-html', function(req, res) {
+  var json = req.body;
+  var height = json.height;
+  var width = json.width;
+  var html = json.html;
+
+  var inject = fs.readFileSync(__dirname + '/../assets/inject.ejs', 'utf8');
+  var renderedInject = ejs.render(inject, { height: height, width: width });
+  $ = cheerio.load(html);
+  $('body').append(renderedInject);
+
+  res.send($.html());
+});
+
+module.exports = router;
