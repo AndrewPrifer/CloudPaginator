ddd0df4f56ee38eb7a1a9ebc32a6cba5e746beb4
Andrew Peter Prifer <prifer.andras@gmail.com>
2016-12-15 18:41:22 +0100

    Unzip and serve uploaded epub files

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,41 @@
-var express = require('express');
-var router = express.Router();
-var fs = require('fs');
-var ejs = require('ejs');
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const mv = require('mv');
+const unzip = require('unzip');
+const ejs = require('ejs');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 
-var cheerio = require('cheerio');
+const cheerio = require('cheerio');
 
 router.post('/', function(req, res) {
   res.redirect('/paginate-html');
 });
 
 router.post('/paginate-html', function(req, res) {
-  var json = req.body;
-  var height = json.height;
-  var width = json.width;
-  var html = json.html;
+  const json = req.body;
+  const height = json.height;
+  const width = json.width;
+  const html = json.html;
 
-  var inject = fs.readFileSync(__dirname + '/../assets/inject.ejs', 'utf8');
-  var renderedInject = ejs.render(inject, { height: height, width: width });
+  const inject = fs.readFileSync('assets/inject.ejs', 'utf8');
+  const renderedInject = ejs.render(inject, {height: height, width: width});
   $ = cheerio.load(html);
   $('body').append(renderedInject);
 
   res.send($.html());
+});
+
+router.post('/paginate-epub', upload.single('epub'), function(req, res) {
+  const epubPath = `${req.file.path}.epub`;
+  fs.renameSync(req.file.path, epubPath);
+  fs.createReadStream(epubPath).pipe(unzip.Extract({ path: path.join('public/epub', req.file.filename) }));
+
+  // TODO create reader
+
+  res.send({ path: `/${req.file.filename}` });
 });
 
 module.exports = router;
