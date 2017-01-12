ef04f15b51a9f2c0521e064aba031c0ccf47cb3f
Andrew Peter Prifer <prifer.andras@gmail.com>
2017-01-12 19:19:01 +0100

    Return inlined chapter array

--- routes/index.js
+++ routes/index.js
@@ -6,36 +6,66 @@ const mv = require('mv');
 const unzip = require('unzip');
 const ejs = require('ejs');
 const multer = require('multer');
-const upload = multer({dest: 'uploads/'});
-
+const upload = multer({ dest: 'uploads/' });
 const cheerio = require('cheerio');
+const Inliner = require('inliner');
+const EPub = require('epub');
 
-router.post('/', function(req, res) {
+router.post('/', function (req, res) {
   res.redirect('/paginate-html');
 });
 
-router.post('/paginate-html', function(req, res) {
+router.post('/paginate-html', function (req, res) {
   const json = req.body;
   const height = json.height;
   const width = json.width;
   const html = json.html;
 
   const inject = fs.readFileSync('assets/inject.ejs', 'utf8');
-  const renderedInject = ejs.render(inject, {height: height, width: width});
+  const renderedInject = ejs.render(inject, { height: height, width: width });
   $ = cheerio.load(html);
   $('body').append(renderedInject);
 
   res.send($.html());
 });
 
-router.post('/paginate-epub', upload.single('epub'), function(req, res) {
+/**
+ * This endpoint accepts an epub file, inlines the chapters and sends them back.
+ */
+router.post('/reader', upload.single('epub'), function (req, res) {
+  // append .epub extension to generated filename
   const epubPath = `${req.file.path}.epub`;
   fs.renameSync(req.file.path, epubPath);
-  fs.createReadStream(epubPath).pipe(unzip.Extract({ path: path.join('public/epub', req.file.filename) }));
 
-  // TODO create reader
+  const reader = new EPub(epubPath);
 
-  res.send({ path: `/${req.file.filename}` });
+  reader.on('end', () => {
+    fs.createReadStream(epubPath)
+      .pipe(unzip.Extract({ path: req.file.path }))
+      .on('close', () => {
+        Promise.all(reader.flow.map(e => inline(path.join(req.file.path, e.href))))
+          .then((htmls) => {
+            console.log('done');
+            res.send(htmls);
+          });
+      });
+  });
+  reader.parse();
 });
+
+/**
+ * Promise version of the inliner.
+ */
+function inline(path) {
+  return new Promise((resolve, reject) => {
+    const inliner = new Inliner(path, (error, html) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(html);
+      }
+    });
+  });
+}
 
 module.exports = router;
